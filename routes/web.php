bd6ae54be7cb9b9876768f7c501368649f95679c
annisaarsylia <annisaarsylia@gmail.com>
2023-05-25 15:39:42 +0700

    send mail

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,18 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('send-mail', function () {
+   
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+   
+    \Mail::to('your_receiver_email@gmail.com')->send(new \App\Mail\MyTestMail($details));
+   
+    dd("Email is Sent.");
+});
+
 // Route::controller(SesiController::class)->group(function(){
 //     Route::get('/sesi/{id}','show');
 //     Route::get('/whosesi/{id}','showNow');
